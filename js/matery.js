6c09d39ea3f61df3906ba105caf50bd39c74e9fe
summerbird <2718075060@qq.com>
2023-10-31 12:43:45 +0800

    Site updated: 2023-10-31 12:43:42

--- js/matery.js
+++ js/matery.js
@@ -120,7 +120,7 @@ $(function () {
 
     /*回到顶部*/
     $('#backTop').click(function () {
-        $('body,html').animate({scrollTop: 0}, 400);
+        $('body,html').animate({ scrollTop: 0 }, 400);
         return false;
     });
 
@@ -146,27 +146,63 @@ $(function () {
         }
     }
 
-    	
-	$(".nav-menu>li").hover(function(){
-		$(this).children('ul').stop(true,true).show();
-		 $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
-		
-	},function(){
-		$(this).children('ul').stop(true,true).hide();
-		$('.nav-item.nav-show').removeClass('nav-show');
-	})
-	
-    $('.m-nav-item>a').on('click',function(){
-            if ($(this).next('ul').css('display') == "none") {
-                $('.m-nav-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(100);
-                $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
-            }else{
-                $(this).next('ul').slideUp(100);
-                $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
-            }
+
+    $(".nav-menu>li").hover(function () {
+        $(this).children('ul').stop(true, true).show();
+        $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
+
+    }, function () {
+        $(this).children('ul').stop(true, true).hide();
+        $('.nav-item.nav-show').removeClass('nav-show');
+    })
+
+    $('.m-nav-item>a').on('click', function () {
+        if ($(this).next('ul').css('display') == "none") {
+            $('.m-nav-item').children('ul').slideUp(300);
+            $(this).next('ul').slideDown(100);
+            $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
+        } else {
+            $(this).next('ul').slideUp(100);
+            $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
+        }
     });
 
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+// 深色模式按钮设置
+if (localStorage.getItem('dark') === '1') {
+    document.body.classList.add('dark');
+} else if (new Date().getHours() >= 22 || new Date().getHours() < 7) {
+    /*定时开启暗色模式<默认晚22点至早6点默认开启>*/
+    // document.body.classList.add('dark');
+    // $("#nightMode").removeClass("fa-moon-o").addClass("fa-lightbulb");
+} else if (matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark');
+}
+
+// 深色模式设置
+function switchNightMode() {
+    var body = document.body;
+    if (body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        localStorage.setItem('dark', '0');
+        $('#nightMode').removeClass("fa-lightbulb").addClass("fa-moon-o");
+        return;
+    } else {
+        document.body.classList.add('dark');
+        localStorage.setItem('dark', '1');
+        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb");
+        return;
+    }
+}
+
+/*提醒开启夜间模式功能*/
+setTimeout(
+    function () {
+        if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+            let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>'
+                + '<i class="fa fa-bell" aria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)/</span>'
+            M.toast({ html: toastHTML })
+        }
+    }, 2200);
